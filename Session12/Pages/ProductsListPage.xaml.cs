a497ee6757a0f7e80bf5f332f04fc0c7af0e4f66
Ruslan <Ruslan@DESKTOP-NR8DVE4>
2022-12-02 17:57:59 +0300

    почти доделал логин и начал делать список проудктов

---
+++ Session12/Pages/ProductsListPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Session12.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductsListPage.xaml
+    /// </summary>
+    public partial class ProductsListPage : Page
+    {
+        #region DependencyPropertyes
+
+        public ObservableCollection<Product> Products
+        {
+            get { return (ObservableCollection<Product>)GetValue(ProductsProperty); }
+            set { SetValue(ProductsProperty, value); }
+        }
+
+        public static readonly DependencyProperty ProductsProperty =
+            DependencyProperty.Register("Products", typeof(ObservableCollection<Product>), typeof(ProductsListPage));
+
+        #endregion
+
+
+        public ProductsListPage()
+        {
+
+            Products = App.db.Product.Local;
+
+            InitializeComponent();
+        }
+    }
+}
